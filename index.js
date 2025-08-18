0bc66b1cd3f3e8d93e31042fa446894faf38c28c
Eliyahu Kainyah Yalley <46997419+Elikyals@users.noreply.github.com>
2025-08-17 23:13:19 -0400

    Add API key configuration and update fetch request to use it

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-fetch('http://www.omdbapi.com/?s=Merlin&apikey=')
+import { API_KEY } from './config.js'
+fetch(`http://www.omdbapi.com/?s=Merlin&apikey=${API_KEY}`)
     .then(response => response.json())
     .then(data => console.log(data))
